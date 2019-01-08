6cbeaacdce6ae1344238e5749805fd36462c3286
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-08 21:56:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
+<<<<<<< HEAD
       if(i === 1) {
         array.push("I am 1 strange loop.");
       }
@@ -27,4 +28,14 @@ function doWhileLoop(array) {
       incrementVariable()
   } while (array.length > 0 && incrementVariable());
   return array;
+=======
+    if(i===1) {
+      array[i] = "I am 1 strange loop.";
+    }
+    else {
+      array[i] = "I am " + i + " strange loops.";
+    }
+  }
+  return array;
+>>>>>>> 9ee883f3dbb84aedc7852f2c509314df2b8acff3
 }
